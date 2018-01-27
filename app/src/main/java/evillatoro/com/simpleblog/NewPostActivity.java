d50ee9fe1e9c9aa120b005b04e6b917b465b1bcc
Edwin Villatoro <villatoroedwin1@gmail.com>
2018-01-27 12:34:59 -0500

    connected to Firebase Storage and can send pictures to it

--- app/src/main/java/evillatoro/com/simpleblog/NewPostActivity.java
+++ app/src/main/java/evillatoro/com/simpleblog/NewPostActivity.java
@@ -1,20 +1,37 @@
 package evillatoro.com.simpleblog;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ImageView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import org.w3c.dom.Text;
 
 public class NewPostActivity extends AppCompatActivity {
 
     private ImageButton mSelectImage;
+    private EditText mPostTitle;
+    private EditText mPostDesc;
+    private Button mSubmitBtn;
+
+    private Uri mImageUri = null;
+
     private static final int GALLERY_REQUEST = 1;
+
+    private StorageReference mStorage;
+    private ProgressDialog mProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +40,15 @@ public class NewPostActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mStorage = FirebaseStorage.getInstance().getReference();
+
         mSelectImage = (ImageButton) findViewById(R.id.imageSelect);
+
+        mPostTitle = (EditText) findViewById(R.id.titleField);
+        mPostDesc = (EditText) findViewById(R.id.descField);
+        mSubmitBtn = (Button) findViewById(R.id.submitBtn);
+
+        mProgress = new ProgressDialog(this);
 
         mSelectImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +58,33 @@ public class NewPostActivity extends AppCompatActivity {
                 startActivityForResult(galleryIntent, GALLERY_REQUEST);
             }
         });
+
+        mSubmitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startPosting();
+            }
+        });
+    }
+
+    private void startPosting() {
+        mProgress.setMessage("Posting to Blog...");
+
+        String title_val = mPostTitle.getText().toString().trim();
+        String desc_val = mPostDesc.getText().toString().trim();
+
+        if (!TextUtils.isEmpty(title_val) && !TextUtils.isEmpty(desc_val) && mImageUri != null) {
+            mProgress.show();
+            StorageReference filepath = mStorage.child("Blog_Images").child(mImageUri.getLastPathSegment());
+
+            filepath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    mProgress.dismiss();
+                }
+            });
+        }
     }
 
     @Override
@@ -40,9 +92,9 @@ public class NewPostActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if ((requestCode == GALLERY_REQUEST) && (resultCode == RESULT_OK)) {
-            Uri imageUri = data.getData();
+            mImageUri = data.getData();
 
-            mSelectImage.setImageURI(imageUri);
+            mSelectImage.setImageURI(mImageUri);
         }
     }
 }
