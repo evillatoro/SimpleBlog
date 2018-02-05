d87294f8c4402ee58c9666ca536da05d014844d3
Allen Chan <achan41@gatech.edu>
2018-02-05 17:00:55 -0500

    Added loading of blogs on main activity in recyclerview -Added firebaseui and picasso library

--- app/src/main/java/evillatoro/com/simpleblog/MainActivity.java
+++ app/src/main/java/evillatoro/com/simpleblog/MainActivity.java
@@ -1,32 +1,46 @@
 package evillatoro.com.simpleblog;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
     private DatabaseReference mDatabase;
+    private RecyclerView mBlogList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         // connecting to root directory of application
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Blog");
+
+        mBlogList = (RecyclerView) findViewById(R.id.blog_list);
+        mBlogList.setHasFixedSize(true);
+        mBlogList.setLayoutManager(new LinearLayoutManager(this));
+
         final Intent intent = new Intent(this, NewPostActivity.class);
-        mDatabase = FirebaseDatabase.getInstance().getReference();
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.firebase_btn);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,6 +49,60 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerAdapter<Blog, BlogViewHolder> firebaseRecyclerAdapter =
+                new FirebaseRecyclerAdapter<Blog, BlogViewHolder>(
+
+                        Blog.class,
+                        R.layout.blog_row,
+                        BlogViewHolder.class,
+                        mDatabase
+                ) {
+                    @Override
+                    protected void populateViewHolder(BlogViewHolder viewHolder, Blog model, int position) {
+
+                        viewHolder.setTitle(model.getTitle());
+                        viewHolder.setDesc(model.getDesc());
+                        viewHolder.setImage(getApplicationContext(), model.getImage());
+                    }
+                };
+
+        mBlogList.setAdapter(firebaseRecyclerAdapter);
+
+    }
+
+    public static class BlogViewHolder extends RecyclerView.ViewHolder {
+
+        View mView;
+
+        public BlogViewHolder(View itemView) {
+            super(itemView);
+
+            mView = itemView;
+        }
+
+        public void setTitle(String title) {
+
+            TextView post_title = (TextView) mView.findViewById(R.id.post_title);
+            post_title.setText(title);
+        }
+
+        public void setDesc(String desc) {
+
+            TextView post_desc = (TextView) mView.findViewById(R.id.post_desc);
+            post_desc.setText(desc);
+        }
+
+        public void setImage(Context ctx, String image) {
+
+            ImageView postImage = (ImageView) mView.findViewById(R.id.post_image);
+            Picasso.with(ctx).load(image).into(postImage);
+        }
     }
 
     @Override
